64f60ce4155578bfec8dc31afc5a8cc40d939ec0
qiang <guyan0319@sohu.com>
2020-10-16 18:32:15 +0800

    no message

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
+import "go-websocket/conf"
+
 func main() {
-InitConfig()
-
-
-
+	//初始化配置
+	InitConfig()
 
 }
 
 func InitConfig() {
-
 	c := conf.Config{}
-	c.Routes=[]string{"/ping","/renewal","/login","/login/mobile","/sendsms","/signup/mobile","/signup/mobile/exist"}
-	c.OpenJwt=true//开启jwt
+	c.Routes = []string{"/ping", "/renewal", "/login", "/login/mobile", "/sendsms", "/signup/mobile", "/signup/mobile/exist"}
+	c.OpenJwt = true //开启jwt
 	conf.Set(c)
-	//初始化数据验证
-	handle.InitValidate()
 }
